4b1f0ab70f9a232845850dfe2a320b8eb03c3521
Lukas Hernandez <ptownlukas@yahoo.com>
2024-06-10 12:23:08 -0400

    Initial Commit

---
+++ Soundtrack Your Life/Soundtrack_Your_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soundtrack_Your_LifeApp.swift
+//  Soundtrack Your Life
+//
+//  Created by Lukas Hernandez on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Soundtrack_Your_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
